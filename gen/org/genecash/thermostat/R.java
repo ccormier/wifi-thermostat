1f1419cfa2cc03cc88871e65ad17d34ef8da1cb9
Gene Cash <gene.cash@gmail.com>
2013-03-28 22:13:02 -0400

    new icon, rename to wifi thermostat

--- gen/org/genecash/thermostat/R.java
+++ gen/org/genecash/thermostat/R.java
@@ -11,19 +11,22 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int tstat_icon=0x7f020000;
+        public static final int thermostat_icon=0x7f020000;
     }
     public static final class id {
-        public static final int layout_cool=0x7f040001;
-        public static final int layout_heat=0x7f040004;
-        public static final int mode=0x7f040000;
-        public static final int status=0x7f040007;
-        public static final int table_cool=0x7f040002;
-        public static final int table_heat=0x7f040005;
-        public static final int update_cool=0x7f040003;
-        public static final int update_heat=0x7f040006;
+        public static final int layout_cool=0x7f050001;
+        public static final int layout_heat=0x7f050004;
+        public static final int mode=0x7f050000;
+        public static final int status=0x7f050007;
+        public static final int table_cool=0x7f050002;
+        public static final int table_heat=0x7f050005;
+        public static final int update_cool=0x7f050003;
+        public static final int update_heat=0x7f050006;
     }
     public static final class layout {
         public static final int thermostat=0x7f030000;
+    }
+    public static final class string {
+        public static final int app_name=0x7f040000;
     }
 }
